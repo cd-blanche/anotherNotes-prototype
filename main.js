0c9f91576706f6b1c01dedb370be8a4281179dec
cd-blanche <79345528+cd-blanche@users.noreply.github.com>
2023-03-21 17:00:57 -0400

    Add drag function to notes

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-function moveThings() {
-
+function load() {
   const notes = document.querySelectorAll('.anote');
 
   notes.forEach(note => {
     note.addEventListener('mousedown', dragNote)
-  })
-
-  function dragNote(e) {
-    console.log(e.target)
-    e.target.onmousemove = () => {
-      console.log('moving');
-    };
-    e.target.onmouseup = () => {
-      e.target.onmousemove = null;
-      e.target.onmouseup = null;
-      console.log('stopped moving');
-    };
+  });
+};
+function dragNote(e) {
+  document.onmousemove = (event) => {
+    e.target.parentNode.style.top = `${event.y - e.layerY}px`
+    e.target.parentNode.style.left = `${event.x - e.layerX}px`
+  };
+  document.onmouseup = () => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+    console.log('stopped moving');
   };
 };
-
-moveThings();
+load();

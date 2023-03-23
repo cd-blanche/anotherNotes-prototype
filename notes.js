45de81aa77a177020e7d673303e82af645169b06
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-03-23 00:00:39 -0400

    Add new note button basic functionality

--- notes.js
+++ notes.js
@@ -1,15 +1,33 @@
 class Note {
-  constructor(title = 'Sample Title', body = 'Sample Body', positionX = 20, positionY = 10) {
+
+  constructor(id = (Math.floor(Math.random() * 10000) + 1),title = 'Sample Title', body = 'Sample Body', positionX = 20, positionY = 10) {
+    this.id = id,
     this.title = title,
     this.body = body,
     this.positionX = positionX,
     this.positionY = positionY
-  }
+  };
 
-}
+};
+
 
 function createNote() {
-  return new Note();
-}
+  const targetBody = document.querySelector('#main');
+  const note = new Note();
+  console.log(note);
 
-console.log(createNote())
+  const appendNote = `
+  <div class="anote" id="anote-${note.id}">
+    <div class="anote-header"></div>
+    <div class="anote-content">
+      <input type="text" class="anote-title" value="${note.title}">
+      <textarea class="anote-body" cols="30" rows="10">${note.body}</textarea>
+    </div>
+  </div>
+  `
+  targetBody.innerHTML += appendNote;
+
+};
+
+const addNote = document.querySelector('#add-note');
+addNote.addEventListener('click', createNote)
